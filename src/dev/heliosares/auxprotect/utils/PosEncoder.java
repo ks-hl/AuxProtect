d2e0c67801874d09f7251857f6aa2ff179b20c68
Heliosares <kyler.t.skeens@gmail.com>
2023-02-28 21:04:35 -0500

    Fix overflow

--- src/dev/heliosares/auxprotect/utils/PosEncoder.java
+++ src/dev/heliosares/auxprotect/utils/PosEncoder.java
@@ -177,9 +177,9 @@ public class PosEncoder {
      * X and Z are stored in 8ths, Y is stored in 4ths.
      */
     public static byte getFractionalByte(double dx, double dy, double dz) {
-        int x = (int) Math.min(Math.round((dx % 1) * 8), 7) << 5;
-        int y = (int) Math.min(Math.round((dy % 1) * 4), 3) << 3;
-        int z = (int) Math.min(Math.round((dz % 1) * 8), 7);
+        int x = (int) Math.min(Math.round((Math.abs(dx % 1)) * 8), 7) << 5;
+        int y = (int) Math.min(Math.round((Math.abs(dy % 1)) * 4), 3) << 3;
+        int z = (int) Math.min(Math.round((Math.abs(dz % 1)) * 8), 7);
 
         return (byte) (x | y | z);
     }
