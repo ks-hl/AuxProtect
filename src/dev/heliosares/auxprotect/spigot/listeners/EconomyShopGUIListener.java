56d955f5307e9ca8bba150c1a87108469ff48c29
Heliosares <kyler.t.skeens@gmail.com>
2023-03-07 16:45:18 -0500

    Update EconomyShopGUI, needs tested

--- src/dev/heliosares/auxprotect/spigot/listeners/EconomyShopGUIListener.java
+++ src/dev/heliosares/auxprotect/spigot/listeners/EconomyShopGUIListener.java
@@ -4,6 +4,7 @@ import dev.heliosares.auxprotect.database.DbEntry;
 import dev.heliosares.auxprotect.database.EntryAction;
 import dev.heliosares.auxprotect.spigot.AuxProtectSpigot;
 import me.gypopo.economyshopgui.api.events.PostTransactionEvent;
+import me.gypopo.economyshopgui.objects.ShopItem;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -18,14 +19,16 @@ public class EconomyShopGUIListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPostTransactionEvent(PostTransactionEvent e) {
-        ItemStack item = e.getItemStack();
-        boolean state = e.getTransactionType().toString().contains("BUY");
-        String data = "ESG, " + plugin.formatMoney(e.getPrice()) + ", QTY: " + item.getAmount();
-        if (plugin.getEconomy() != null) {
-            data += ", bal: " + plugin.formatMoney(plugin.getEconomy().getBalance(e.getPlayer()));
+        for (ShopItem shopItem : e.getItems().keySet()) {
+            ItemStack item = shopItem.getItemToGive();
+            boolean state = e.getTransactionType().toString().contains("BUY");
+            String data = "ESG, " + plugin.formatMoney(e.getPrice()) + ", QTY: " + item.getAmount();
+            if (plugin.getEconomy() != null) {
+                data += ", bal: " + plugin.formatMoney(plugin.getEconomy().getBalance(e.getPlayer()));
+            }
+            DbEntry entry = new DbEntry(AuxProtectSpigot.getLabel(e.getPlayer()), EntryAction.SHOP, state,
+                    e.getPlayer().getLocation(), item.getType().toString().toLowerCase(), data);
+            plugin.add(entry);
         }
-        DbEntry entry = new DbEntry(AuxProtectSpigot.getLabel(e.getPlayer()), EntryAction.SHOP, state,
-                e.getPlayer().getLocation(), item.getType().toString().toLowerCase(), data);
-        plugin.add(entry);
     }
 }
