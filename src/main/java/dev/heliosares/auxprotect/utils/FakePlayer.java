dfa7a31326bbbb30f5b66277a1b505cce4cf3a55
Heliosares <kyler.t.skeens@gmail.com>
2023-06-06 11:51:11 -0400

    Update FakePlayer for API change

--- src/main/java/dev/heliosares/auxprotect/utils/FakePlayer.java
+++ src/main/java/dev/heliosares/auxprotect/utils/FakePlayer.java
@@ -76,7 +76,7 @@ public class FakePlayer {
         if (skin != null) profile.getProperties().put("textures", skin.wrap());
         // TODO fake player still being added to list
         packet.getPlayerInfoDataLists().write(1, List.of(
-                new PlayerInfoData(uuid, 0, false, EnumWrappers.NativeGameMode.SURVIVAL, profile, WrappedChatComponent.fromLegacyText(name), null)
+                new PlayerInfoData(uuid, 0, false, EnumWrappers.NativeGameMode.SURVIVAL, profile, WrappedChatComponent.fromLegacyText(name), (WrappedRemoteChatSessionData) null)
         ));
         protocol.sendServerPacket(audience, packet);
 
