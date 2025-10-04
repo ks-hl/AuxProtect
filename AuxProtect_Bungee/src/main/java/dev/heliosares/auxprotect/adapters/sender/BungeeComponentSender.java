e6a702dd1e7186a47f2a4398b57162d984af89d4
ks-hl <8331595+ks-hl@users.noreply.github.com>
2025-10-04 18:38:57 -0400

    Remove Bungee support

--- AuxProtect_Bungee/src/main/java/dev/heliosares/auxprotect/adapters/sender/BungeeComponentSender.java
+++
@@ -1,7 +0,0 @@
-package dev.heliosares.auxprotect.adapters.sender;
-
-import net.md_5.bungee.api.chat.BaseComponent;
-
-public interface BungeeComponentSender {
-    void sendMessage(BaseComponent... component);
-}
